ab9fc8cfff5b42cb0ab3824e67ed4c23c0216c37
Kaviyeah <24f1000772@ds.study.iitm.ac.in>
2026-07-07 21:14:11 +0530

    Added all GA endpoints

---
+++ config.py
@@ -0,0 +1,11 @@
+EMAIL = "24f1000772@ds.study.iitm.ac.in"
+
+AIPIPE_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6IjI0ZjEwMDA3NzJAZHMuc3R1ZHkuaWl0bS5hYy5pbiIsImlhdCI6MTc4MjYyMTcyOCwiaXNzIjoiaHR0cHM6Ly9haXBpcGUub3JnIiwiYXVkIjoiYWlwaXBlLWFwaSIsImV4cCI6MTc4MzIyNjUyOH0.R_WoaChkH-AvrbE4k2IPGLsF4-H9N2AZZM6gDsekxP0"
+
+AIPIPE_BASE = "https://aipipe.org/openai/v1"
+
+TEXT_MODEL = "gpt-4o-mini"
+
+VISION_MODEL = "gpt-4o"
+
+EMBED_MODEL = "text-embedding-3-small"
